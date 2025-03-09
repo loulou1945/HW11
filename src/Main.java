55aec5a0bf4c5aca0a65cd61f4f6d3baa8264d74
loulou1945 <smirnova_vika2014@mail.ru>
2025-03-09 21:23:36 +0300

    Finished hw

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Book charlesDickens = new Book("The old curiosity shop", new Author("Charles","Dickens"), 1840);
-        Book viktorHugo = new Book("Ninety-three",new Author("Victor","Hugo"),1874);
+        Book charlesDickens = new Book("The old curiosity shop", new Author("Charles", "Dickens"), 1840);
+        Book viktorHugo = new Book("Ninety-three", new Author("Victor", "Hugo"), 1874);
 
-        Author pushkin = new Author("Aleksandr","Pushkin");
-        Author lev = new Author("Lev","Tolstoy");
+        Author pushkin = new Author("Aleksandr", "Pushkin");
+        Author lev = new Author("Lev", "Tolstoy");
 
         charlesDickens.setPublishingYear(1841);
+
     }
 }
